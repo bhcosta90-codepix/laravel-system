dfbc39b3514c969d375e94398787e9a1860b26ef
Bruno Costa <bruno.costa@dafiti.com.br>
2023-10-27 14:58:19 -0300

    wip

--- app/Services/RabbitMQService.php
+++ app/Services/RabbitMQService.php
@@ -6,18 +6,40 @@ namespace App\Services;
 
 use App\Services\Contracts\AMQPInterface;
 use App\Services\Contracts\RabbitMQInterface;
+use Bschmitt\Amqp\Facades\Amqp;
 use Closure;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class RabbitMQService implements AMQPInterface, RabbitMQInterface
 {
+    public function __construct(){
+        //
+    }
+
     public function publish($name, array $value = []): void
     {
-        dump('TODO: Implement publish() method.');
+        Amqp::publish($name, json_encode($value));
     }
 
     public function consume(string $queue, array|string $topic, Closure $closure, array $custom = []): void
     {
-        dump('TODO: Implement consume() method.');
+        if (is_string($topic)) {
+            $topic = [$topic];
+        }
+
+        $topic = [
+            'routing' => $topic,
+        ];
+
+        Amqp::consume($queue, function ($message, $resolver) use ($queue, $closure) {
+            try {
+                $closure($message->body);
+                $resolver->acknowledge($message);
+            } catch (Throwable $e) {
+                Log::error("Error consumer {$queue}: " . $e->getMessage() . json_encode($e->getTrace()));
+            }
+        }, $custom + $topic);
     }
 
 }
