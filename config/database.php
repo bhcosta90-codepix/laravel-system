6d0ae83d49b93dae7132a23b2d47b84f11db0ad9
Bruno Costa <bruno.costa@dafiti.com.br>
2023-11-17 15:41:35 -0300

    wip

--- config/database.php
+++ config/database.php
@@ -74,7 +74,7 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
-            'search_path' => 'system',
+            'search_path' => env('DB_SCHEMA', 'public'),
             'sslmode' => 'prefer',
         ],
 
